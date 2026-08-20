86934022d37a037ee08b945ee95240fadde3e315
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-20 22:22:24 +0100

    Add quitting and closing windows keys

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -30,6 +30,10 @@ vim.diagnostic.config {
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+vim.keymap.set('n', '<leader>qq', '<Cmd>qa<CR>', { desc = 'Quit Neovim' })
+
+vim.keymap.set('n', '<leader>wd', '<Cmd>q<CR>', { desc = 'Close Window' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
