005e6127e12d860183a89ac38f1eb7076847b4f4
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-25 17:47:28 +0100

    Add diagnostics config

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ require 'lsp'
 require 'format'
 require 'autocomplete'
 require 'treesitter'
+require 'diagnostics'
 require 'plugins'
 
 vim.g.start_time = vim.uv.hrtime() -- Used for dashboard to display startup time
