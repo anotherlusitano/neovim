a12f05d439c450fdc09474df966b29777cd9f513
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-25 17:42:45 +0100

    Add autocomplete config

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ require 'pack'
 require 'mason_config'
 require 'lsp'
 require 'format'
+require 'autocomplete'
 require 'plugins'
 
 vim.g.start_time = vim.uv.hrtime() -- Used for dashboard to display startup time
---
+++ lua/autocomplete.lua
@@ -0,0 +1,37 @@
+vim.pack.add { { src = 'https://github.com/L3MON4D3/LuaSnip', version = vim.version.range '2.*' } }
+
+---@diagnostic disable-next-line: need-check-nil
+require('luasnip').setup {}
+
+vim.pack.add { 'https://github.com/rafamadriz/friendly-snippets' }
+require('luasnip.loaders.from_vscode').lazy_load()
+
+vim.pack.add { { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range '1.*' } }
+require('blink.cmp').setup {
+  keymap = {
+    preset = 'default',
+
+    -- I prefer this keymaps
+    ['<CR>'] = { 'select_and_accept', 'fallback' },
+    ['<Tab>'] = { 'select_next', 'fallback' },
+    ['<S-Tab>'] = { 'select_prev', 'fallback' },
+  },
+
+  appearance = {
+    nerd_font_variant = 'mono',
+  },
+
+  completion = {
+    documentation = { auto_show = false, auto_show_delay_ms = 500 },
+  },
+
+  sources = {
+    default = { 'lsp', 'path', 'snippets' },
+  },
+
+  snippets = { preset = 'luasnip' },
+
+  fuzzy = { implementation = 'lua' },
+
+  signature = { enabled = true },
+}
