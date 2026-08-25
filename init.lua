1d5daca5e56fc0e07df492d6252e781ee5b659f3
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-25 17:38:13 +0100

    Add format config

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require 'keymaps'
 require 'pack'
 require 'mason_config'
 require 'lsp'
+require 'format'
 require 'plugins'
 
 vim.g.start_time = vim.uv.hrtime() -- Used for dashboard to display startup time
---
+++ lua/format.lua
@@ -0,0 +1,26 @@
+vim.pack.add { 'https://github.com/stevearc/conform.nvim' }
+
+require('conform').setup {
+  notify_on_error = false,
+  format_on_save = function(bufnr)
+    -- You can specify filetypes to autoformat on save here:
+    local enabled_filetypes = { lua = true }
+
+    if enabled_filetypes[vim.bo[bufnr].filetype] then
+      return { timeout_ms = 500 }
+    else
+      return nil
+    end
+  end,
+  default_format_opts = {
+    lsp_format = 'fallback',
+  },
+
+  formatters_by_ft = {
+    lua = { 'stylua' },
+  },
+}
+
+vim.keymap.set({ 'n', 'v' }, '<leader>f', function() require('conform').format { async = true } end, {
+  desc = '[F]ormat buffer',
+})
