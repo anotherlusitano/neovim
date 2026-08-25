9db09b6cfbf84d243c2af28b9f378b7e530d862c
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2026-08-25 17:44:59 +0100

    Add Tree-sitter configt

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ require 'mason_config'
 require 'lsp'
 require 'format'
 require 'autocomplete'
+require 'treesitter'
 require 'plugins'
 
 vim.g.start_time = vim.uv.hrtime() -- Used for dashboard to display startup time
